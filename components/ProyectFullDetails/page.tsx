4e840c66c30c9db743088abfd6c093ee8d18c245
unknown <fandinofacundo123@gmail.com>
2025-02-27 09:27:13 -0300

    Proyect details now are responsive

--- components/ProyectFullDetails/page.tsx
+++ components/ProyectFullDetails/page.tsx
@@ -30,79 +30,82 @@ export default function ProyectFullDetails({ name, url, backgroundColor,
             className={`w-full h-full flex justify-center items-center`}
             >
                 <div
-                className={`${backgroundColor} w-4/5 h-5/6 overflow-y-auto rounded-lg 
-                            scrollbar ${scrollbarStyle} shadow-lg shadow-black px-6 pb-7`}
+                className={`${backgroundColor} w-4/5 h-5/6 overflow-y-auto lg:rounded-lg 
+                            scrollbar ${scrollbarStyle} shadow-lg shadow-black px-6 pb-7
+                            max-lg:w-full max-lg:h-full`}
                 >
                     <nav className="flex justify-between items-center py-12 px-7">
                         <h1
-                        className={`text-5xl ${fontColor} font-bold tracking-widest`}
+                        className={`text-5xl ${fontColor} font-bold tracking-widest
+                                    max-lg:hidden`}
                         >
                             {name}
                         </h1>
 
                         <button
                         onClick={() => setIsSelected(null)}
-                        className="hover:scale-125 transition-all duration-300"
+                        className="hover:scale-125 transition-all duration-300 max-lg:fixed right-7"
+
                         >
                             <Image 
                             width={200} 
                             height={200}
                             alt="proyect"
                             src={svg}
-                            className="w-14 max-sm:w-20"
+                            className="w-14 max-sm:w-16"
                             />
                         </button>
                     </nav>
                     <div 
-                    className="h-auto w-full flex"
+                    className="h-auto w-full flex max-lg:flex-col"
                     >
                         <div
-                        className="h-auto w-2/4 px-7"
+                        className="h-auto w-2/4 px-7 max-lg:w-full"
                         >
                             <p
-                            className={`${fontColor} text-3xl font-semibold tracking-widest`}
+                            className={`${fontColor} text-xl font-semibold tracking-widest`}
                             >
                                 {description}
                             </p>
 
                             <h2
-                            className={`${fontColor} text-4xl font-semibold tracking-widest mt-16 mb-7`}>
+                            className={`${fontColor} text-2xl font-semibold tracking-widest mt-16 mb-7`}>
                                 Desafio:
                             </h2>
 
                             <p
-                            className={`${fontColor} text-3xl font-semibold tracking-widest`}
+                            className={`${fontColor} text-xl font-semibold tracking-widest`}
                             >
                                 {challenge}
                             </p>
                         </div>
                         <div
-                        className="h-auto w-2/4 flex flex-col items-center"
+                        className="h-auto w-2/4 flex flex-col items-center max-lg:w-full max-lg:mt-16"
                         >
                             <Image
                             src={img}
                             width={200}
                             height={200}
                             alt="proyect image"
-                            className="rounded-lg w-5/6 h-2/4 mb-8 shadow-lg shadow-black"
+                            className="rounded-lg w-full h-2/4 mb-8 shadow-lg shadow-black object-cover"
                             />
 
 <div 
-                            className="mb-16 w-full flex justify-center gap-5"
+                            className="mb-16 w-full flex justify-center gap-5 max-lg:flex-col items-center"
                             >
                                 <a 
                                 className={`${backgroundColor2} px-3 py-6 rounded-lg shadow-md shadow-black w-2/4
                                             text-center hover:scale-90 transition-all duration-300
                                             hover:shadow-none ${fontColor} tracking-widest hover:bg-transparent
                                             text-2xl font-semibold`}
-                                href={url}>
+                                href={url}> 
                                     Deploy
                                 </a>    
                                 <a 
                                 className={`${backgroundColor2} px-3 py-6 rounded-lg shadow-md shadow-black w-2/4
                                             text-center hover:scale-90 transition-all duration-300 ${fontColor}
                                             hover:shadow-none tracking-widest hover:bg-transparent
-                                            text-2xl font-semibold`}
+                                            text-2xl font-semibold max-lg:text-xl`}
                                 href={urlRepo}
                                 >
                                     Repositorio
