41a5251daec5f023259356fbbb459549a75b4e46
Facundo <122570343+facufan07@users.noreply.github.com>
2025-03-07 15:13:38 -0300

    Animations changed

--- components/Welcome/page.tsx
+++ components/Welcome/page.tsx
@@ -1,22 +1,68 @@
+"use client"
 interface WelcomeProps {
     fontColor: string
 }
 
+import { useEffect, useState } from "react"
 import "./style.css"
 import Image from 'next/image' 
 
 export default function Welcome({fontColor}: WelcomeProps){
 
     const border = fontColor === 'text-bHueso' ? 'border-r-2 border-bHueso' : 'border-r-2 border-nCarbon'
-    const borderImage = fontColor === 'text-bHueso' ? 'border-2 border-vMusgo' : 'border-2 border-nCarbon'
+    const borderImage = fontColor === 'text-bHueso' ? 'border-2 border-vMusgo' : 'border-2 border-vSalvia'
+    const fontColorArea = fontColor === 'text-bHueso' ? 'text-vMusgo' : 'text-vSalvia'
 
+    const texts = ["Frontend", "Web"];
+    const [index, setIndex] = useState(0);
+    const [text, setText] = useState("Frontend");
+    const [deleting, setDeleting] = useState(false);
+    const [speed, setSpeed] = useState(100);
+    const [isUseEffectUsed, setIsUseEffectUsed] = useState(false);
+    const [styleAnimation, setStyleAnimation] = useState(`${border} w-[23ch] typing`);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsUseEffectUsed(true);
+            setStyleAnimation("")
+        }, 1000)
+    })
+
+    useEffect(() => {
+        if(isUseEffectUsed){
+            const currentText = texts[index];
+        
+            if (!deleting && text === currentText) {
+                setTimeout(() => setDeleting(true), 1000);
+                return;
+            }
+            
+            if (deleting && text === "") {
+            setDeleting(false);
+            setIndex((prev) => (prev + 1) % texts.length);
+            return;
+            }
+            
+            const timeout = setTimeout(() => {
+                setText((prev) =>
+                deleting
+                    ? currentText.slice(0, prev.length - 1)
+                    : currentText.slice(0, prev.length + 1)
+                );
+                setSpeed(deleting ? 50 : 100);
+            }, speed);
+            
+            return () => clearTimeout(timeout);
+        }
+    }, [text, deleting, index, speed, isUseEffectUsed]);
+    
     return(
-        <section className="flex justify-center items-center h-dvh lg:w-4/5">
-            <div className="flex max-lg:flex-col max-lg:items-center gap-5 animation px-9 
+        <section className="flex justify-center items-center h-dvh lg:w-4/5 animationWelcome">
+            <div className="flex max-lg:flex-col max-lg:items-center gap-5  px-9 
                             ">
                 <Image src="/cvimgF.jpg" 
                 className={`lg:w-1/5 max-lg:w-2/5 max-sm:w-3/5 h-full sm:rounded-s-full max-lg:rounded-full
-                        shadow-md shadow-black ${borderImage}`}
+                        shadow-md shadow-black  px-1 py-1 ${borderImage}`}
                 width={200} 
                 height={200} 
                 alt="Facundo Fandiño"
@@ -29,9 +75,9 @@ export default function Welcome({fontColor}: WelcomeProps){
                         Facundo Fandiño
                     </h1>
                     <h2 className={`sm:text-4xl tracking-widest ${fontColor} mt-10  
-                                    max-sm:text-2xl max-lg:text-4xl whitespace-nowrap ${border}
-                                    w-[23ch] typing font-extralight max-lg:mx-auto`}>
-                        Desarrollador Frontend
+                                    max-sm:text-2xl max-lg:text-4xl whitespace-nowrap  
+                                    ${styleAnimation} font-extralight max-lg:mx-auto`}>
+                        Desarrollador <span className={`${fontColorArea} font-light ${border} blink`}>{text}</span>
                     </h2>
                 </div>
             </div>
