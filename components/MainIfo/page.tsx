a9371a280035a4edcd0f9ae6cc089dfd4cca5277
Facundo <122570343+facufan07@users.noreply.github.com>
2025-03-31 10:29:10 -0300

    Scroll on sections has been fixed

--- components/MainIfo/page.tsx
+++ components/MainIfo/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 interface MainIfoProps {
     fontColor: string;
     backgroundColor: string;
@@ -11,6 +13,7 @@ import Contact from "../Contact/page";
 import Tecnologies from "../Tecnologies/page";
 import Proyects from "../Proyects/page";
 import Trajectory from "../Trajectory/page";
+import { useEffect, useRef } from "react";
 
 export default function MainIfo({fontColor, backgroundColor, section, backgroundColor2, 
                                 }: MainIfoProps) {
@@ -18,16 +21,25 @@ export default function MainIfo({fontColor, backgroundColor, section, background
     const scrollbarStyle = fontColor === 'text-bHueso' ? 'scrollbar-thumb-bHueso' : 'scrollbar-thumb-nCarbon'
     const scrollbarTrack = fontColor === 'text-bHueso' ? 'scrollbar-track-nSuave' : 'scrollbar-track-bSuave'
 
+    const mainRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (mainRef.current) {
+            mainRef.current.scrollTop = 0;
+        }
+    }, [section]);
+
     return(
         <section 
         className={`lg:w-4/5 transition-all duration-800
                     flex flex-col items-center lg:justify-center
-                    max-sm:h-5/6 max-lg:overflow-y-auto 
+                    max-sm:h-5/6 
                     max-lg:py-4 max-lg:mx-auto 
                     max-sm:w-full max-sm:overflow-x-hidden
-                    lg:overflow-y-auto lg:overflow-x-hidden scrollbar
+                    overflow-y-auto lg:overflow-x-hidden scrollbar
                     ${scrollbarStyle} ${scrollbarTrack}`}
         id="mainIfo"
+        ref={mainRef}
         >
 
             {section === '' && (
